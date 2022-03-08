c90d9a2b7e7a02e632be968919f1b3cdce5d7c4f
16syms_g <georgessyms@gmail.com>
2022-03-08 09:49:00 +0000

    6 and 2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i < 10100; i++){
-            if ((i % 3 == 0) && (i % 5 == 0)) {
+            if ((i % 2 == 0) && (i % 6 == 0)) {
                 System.out.print("fizzBuzz");
-            } else if (i % 3 == 0) {
+            } else if (i % 2 == 0) {
                 System.out.print("fizz");
-            } else if (i % 5 == 0) {
+            } else if (i % 6 == 0) {
                 System.out.print("buzz");
             } else{
                 System.out.print(i);
